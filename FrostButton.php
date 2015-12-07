382992459b85482bce55c2a36cb2306363e4935b
Frostsoft <frostsoft@hotmail.com>
2015-12-06 23:31:31 -0800

    Added New Method [FrostButton class]
    Added a multi-time handler in FrostButton class under the
    FrostTimeButton Method.

--- FrostButton.php
+++ FrostButton.php
@@ -5,11 +5,21 @@
 Function FrostTimeButton($start, $end){
 													//Param #1: Time Button Starts To Show.
 													//Param #2: Time Button Will Disapear.
-
 	$now = date('G',time());						//Get Time in Integer Format
 	$Image_path = "YOURIMAGE.PNG";					//Set This Property To Your Image
 	if($now >= $start && $now <= $end){ 
     	echo "<img src=\"$Image_path\">";
+	}
+}
+
+Function FrostTimeButton($s1, $e1, $s2, $e2){
+													//Second method for multi-time support
+	$now = date('G',time());						//Get Time in Integer Format
+	$Image_path = "YOURIMAGE.PNG";					//Set This Property To Your Image
+	if($now >= $s1 && $now <= $e1){ 
+    	echo "<img src=\"$Image_path\">";
+	}else if($now >= $s2 && $now <= $e2){
+		echo "<img src=\"$Image_path\">";
 	}
 }
 
